87d6d52f358fc6bd19cb282359513296913d7da4
ozzyozz112194 <osvaldo.munoz@alumnos.udg.mx>
2020-10-16 17:18:43 -0500

    Practica Slices

--- main.go
+++ main.go
@@ -2,6 +2,35 @@ package main
 
 import "fmt"
 
-func main()  {
-	fmt.Println("Hola Mundo!") // eliminar esta línea
+func main() {
+
+	var number1 int
+	var number2 int
+	var number3 int
+	var total int = 0
+
+	fmt.Println("Cuantos numeros deseas")
+	fmt.Scanln(&number1)
+	number3 = number1 - 1
+
+	//slice
+	s := make([]int, 0, number3)
+	i := 0
+
+	for number3 >= i {
+
+		fmt.Println("Ingresa los numeros")
+		fmt.Scanln(&number2)
+		total += number2
+		s = append(s, number2)
+
+		i++
+
+	}
+
+	for _, v := range s {
+		fmt.Println(v)
+	}
+	print(total)
+
 }
